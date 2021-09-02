6663963d84e435a333094563cb4ed64cd1493eb9
tonyarice <tonyacrice@gmail.com>
2021-09-02 11:58:13 -0400

    Changed label input to number, began adding JS

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
+const amount = document.getElementById('amount');
+const guests = document.getElementById('guests');
+const quality = document.getElementById('quality');
+const tipAmount = document.getElementById('tip-amount');
+
+
+
+
+
+
 function calculate() {
-    alert("Hi");
+    const tip = ((amount.value * quality.value)) / (guests.value).toFixed(2);
+    amount.value = '';
+    guests.value = '';
+    quality.value = '';
+    
+    if(tip === NaN) {
+        tipAmount.innerHTML = "Tip $0 each";
+        showTipAmount();
+
+    } else {
+        tipAmount.innerHTML = "Tip $" + tip + " each";
+        showTipAmount();
+    }
+
+    
+    // alert("Hi");
 }
 
 let button = document.querySelector("#calc-button");
